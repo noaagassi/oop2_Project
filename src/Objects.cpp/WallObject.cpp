697ecd4242033fdd356c1d0919249a3b0a1bd361
רועי יוניוב <roey19630@gmail.com>
2024-06-24 19:04:58 +0300

    .

--- src/Objects.cpp/WallObject.cpp
+++ src/Objects.cpp/WallObject.cpp
@@ -6,6 +6,7 @@ bool WallObject::m_registerit = FactoryObject::registerit(WALL_OBJ,
     [](const sf::Vector2f& pos) -> std::unique_ptr<BaseObject> {return std::make_unique<WallObject>(pos); });
 
 WallObject::WallObject(const sf::Vector2f& initPosition)
+    :StaticObject(initPosition)
 {
     setObjTexture(WALL_OBJ);
 }
