da269addd0a32f48cf9613c6dea7bf3d09199ad5
רועי יוניוב <roeyyo@edu.hac.ac.il>
2024-07-03 14:32:01 +0300

    fixed sound in cmake

--- src/SoundsHandler.cpp
+++ src/SoundsHandler.cpp
@@ -8,16 +8,16 @@ SoundsHandler::~SoundsHandler() {}
 // loadds the sounds into the buffer array
 SoundsHandler::SoundsHandler()
 {
-//    for (int i = 0; i < SOUND_NUM; i++)
-//    {
-//       // m_buffers[i].loadFromFile(m_soundNames[i]);
-//    }
-//
-//    // matches each sound with it's own buffer
-//    for (int i = 0; i < SOUND_NUM; i++)
-//    {
-//        //m_sounds[i].setBuffer(m_buffers[i]);
-//    }
+    for (int i = 0; i < SOUND_NUM; i++)
+    {
+        m_buffers[i].loadFromFile(m_soundNames[i]);
+    }
+
+    // matches each sound with it's own buffer
+    for (int i = 0; i < SOUND_NUM; i++)
+    {
+        m_sounds[i].setBuffer(m_buffers[i]);
+    }
 }
  
  
@@ -29,5 +29,5 @@ SoundsHandler& SoundsHandler::getInstance()
 
 void SoundsHandler::playSound(Sound_Id id)
 {
-    //m_sounds[id].play();
+    m_sounds[id].play();
 }
