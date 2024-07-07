b36d201da8898b3fc21ac2ceca6ce83361194f15
רועי יוניוב <roey19630@gmail.com>
2024-07-07 15:12:08 +0300

    .

--- src/Objects.cpp/BigSlowEnemyObject.cpp
+++ src/Objects.cpp/BigSlowEnemyObject.cpp
@@ -6,7 +6,7 @@ bool BigSlowEnemyObject::m_registerit = FactoryObject::registerit(ENEMY_1_OBJ,
     [](const sf::Vector2f& pos) -> std::unique_ptr<BaseObject> {return std::make_unique<BigSlowEnemyObject>(pos); });
 //------------------------------------------------------
 BigSlowEnemyObject::BigSlowEnemyObject(const sf::Vector2f& initPosition)
-    :BaseEnemyObject(initPosition,150,150,10)
+    :BaseEnemyObject(initPosition,150,150,6)
 {
 
     setObjTexture(ENEMY_2_OBJ);
